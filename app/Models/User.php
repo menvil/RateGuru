0b7d4cfbefda156c1326622dcc7da07b49caccb6
Ivan Moroz <menvil.menvil@gmail.com>
2026-05-14 18:39:20 +0300

    RG-042: Add avatar url field to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'username', 'email', 'password'])]
+#[Fillable(['name', 'username', 'email', 'avatar_url', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
