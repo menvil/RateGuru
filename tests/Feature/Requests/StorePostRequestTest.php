963ebf43ebb3dda8cb569109f85819805d0a5da1
Ivan Moroz <menvil.menvil@gmail.com>
2026-05-17 17:47:42 +0300

    RG-125: Test post description is optional

--- tests/Feature/Requests/StorePostRequestTest.php
+++ tests/Feature/Requests/StorePostRequestTest.php
@@ -25,3 +25,26 @@ it('requires post image', function () {
     expect($validator->fails())->toBeTrue();
     expect($validator->errors()->has('image'))->toBeTrue();
 });
+
+it('requires image to be a valid image file', function () {
+    $request = new StorePostRequest();
+
+    $validator = Validator::make([
+        'title' => 'Homemade pasta',
+        'image' => UploadedFile::fake()->create('not-image.txt', 10, 'text/plain'),
+    ], $request->rules());
+
+    expect($validator->fails())->toBeTrue();
+    expect($validator->errors()->has('image'))->toBeTrue();
+});
+
+it('allows description to be omitted', function () {
+    $request = new StorePostRequest();
+
+    $validator = Validator::make([
+        'title' => 'Homemade pasta',
+        'image' => UploadedFile::fake()->image('dish.jpg'),
+    ], $request->rules());
+
+    expect($validator->errors()->has('description'))->toBeFalse();
+});
