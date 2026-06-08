f44b39f954c5b8c00c548dce02f5c47aebb95830
Ivan Moroz <menvil.menvil@gmail.com>
2026-06-08 16:28:01 +0300

    RG-715: Create supported locales config

---
+++ config/locales.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'fallback' => env('APP_FALLBACK_LOCALE', 'en'),
+    'supported' => [
+        'en' => ['label' => 'English', 'native' => 'English'],
+        'ru' => ['label' => 'Russian', 'native' => 'Русский'],
+        'bg' => ['label' => 'Bulgarian', 'native' => 'Български'],
+    ],
+];
