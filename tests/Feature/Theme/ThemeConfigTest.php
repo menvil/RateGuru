37f6b9b99c64965590c8df76d9b6f71b61a895bf
Ivan Moroz <menvil.menvil@gmail.com>
2026-06-09 00:56:44 +0300

    RG-735: Create theme config

---
+++ tests/Feature/Theme/ThemeConfigTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use function Pest\Laravel\artisan;
+
+it('has theme config with supported preferences', function () {
+    expect(config('themes.preferences'))->toContain('system');
+    expect(config('themes.preferences'))->toContain('light');
+    expect(config('themes.preferences'))->toContain('dark');
+    expect(config('themes.default'))->toBe('system');
+});
+
+it('has theme config with applied themes', function () {
+    expect(config('themes.applied'))->toContain('light');
+    expect(config('themes.applied'))->toContain('dark');
+    expect(config('themes.applied'))->not->toContain('system');
+});
