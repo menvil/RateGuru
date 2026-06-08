6f7ca1659f850002f479f921634853c7a8c33c30
Ivan Moroz <menvil.menvil@gmail.com>
2026-06-08 20:01:41 +0300

    RG-724: Add ProjectSettings translation fields

--- app/Models/ProjectSettings.php
+++ app/Models/ProjectSettings.php
@@ -11,12 +11,19 @@ class ProjectSettings extends Model
 
     protected $fillable = [
         'site_name',
+        'site_name_translations',
         'site_tagline',
+        'site_tagline_translations',
         'site_description',
+        'site_description_translations',
         'object_singular_name',
+        'object_singular_name_translations',
         'object_plural_name',
+        'object_plural_name_translations',
         'upload_cta_label',
+        'upload_cta_label_translations',
         'feed_title',
+        'feed_title_translations',
         'default_locale',
         'default_theme',
         'default_sort',
@@ -26,5 +33,12 @@ class ProjectSettings extends Model
 
     protected $casts = [
         'feature_flags' => 'array',
+        'site_name_translations' => 'array',
+        'site_tagline_translations' => 'array',
+        'site_description_translations' => 'array',
+        'object_singular_name_translations' => 'array',
+        'object_plural_name_translations' => 'array',
+        'upload_cta_label_translations' => 'array',
+        'feed_title_translations' => 'array',
     ];
 }
