5e0f0873456d6c2ccdf900e3d0e4f9625f4de474
Ivan Moroz <menvil.menvil@gmail.com>
2026-06-08 18:00:57 +0300

    RG-721: Create base translation files

---
+++ lang/en/ui.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'brand' => 'RateGuru',
+    'feed' => [
+        'latest' => 'Latest',
+    ],
+    'post' => [
+        'upload' => 'Upload post',
+        'title' => 'Title',
+        'description' => 'Description',
+    ],
+    'comments' => [
+        'title' => 'Comments',
+        'submit' => 'Submit',
+    ],
+    'share' => [
+        'title' => 'Share',
+    ],
+    'report' => [
+        'title' => 'Report',
+    ],
+    'profile' => [
+        'posts' => 'Posts',
+    ],
+    'settings' => [
+        'language' => 'Language',
+    ],
+    'voting' => [
+        'source' => 'Source',
+        'category' => 'Category',
+        'vote' => 'Vote',
+    ],
+];
